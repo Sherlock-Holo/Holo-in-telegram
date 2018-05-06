6ec79bee903eb85e00171b7c32deabc43b20e3c8
Sherlock Holo <sherlockya@gmail.com>
2018-05-06 11:37:24 +0800

    Rewrite google search request

--- google/search.go
+++ google/search.go
@@ -7,17 +7,29 @@ import (
 )
 
 //const searchUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCn_IE6NM_ATjZ0j5vfXIFlyW-EpGs5gsU&cx=006431901905483214390:i3yxhoqkzo0&num=1&alt=json&q=%E6%94%AF%E4%BB%98%E5%AE%9D"
-const searchUrl = "https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&num=1&alt=json&q=%s"
+const searchUrl = "https://www.googleapis.com/customsearch/v1"
 
 func Search(question string) (Answer, error) {
     key := "AIzaSyCn_IE6NM_ATjZ0j5vfXIFlyW-EpGs5gsU"
     cx := "006431901905483214390:i3yxhoqkzo0"
 
-    resp, err := http.Get(fmt.Sprintf(searchUrl, key, cx, question))
+    request, err := http.NewRequest(http.MethodGet, searchUrl, nil)
 
     if err != nil {
         return Answer{}, err
     }
+
+    query := request.URL.Query()
+
+    query.Add("key", key)
+    query.Add("cx", cx)
+    query.Add("num", "1")
+    query.Add("alt", "json")
+    query.Add("q", question)
+
+    request.URL.RawQuery = query.Encode()
+
+    resp, err := http.DefaultClient.Do(request)
 
     decoder := json.NewDecoder(resp.Body)
 
