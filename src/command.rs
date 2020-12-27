6a57176b787138928bf0565dfcb1d948c2835946
Sherlock Holo <sherlockya@gmail.com>
2020-12-27 10:26:34 +0800

    feat: when query name is empty, reply help message

--- src/command.rs
+++ src/command.rs
@@ -4,22 +4,19 @@ use teloxide::utils::command::{BotCommand, ParseError};
 #[command(rename = "lowercase", parse_with = "split")]
 pub enum Command {
     #[command(
-        description = "*/arch* `package [repo]`, repo: eg: `stable` , `testing`, `aur` or `core` , `extra`",
-        parse_with = "parse_arch"
+    description = "*/arch* `package [repo]`, repo: eg: `stable` , `testing`, `aur` or `core` , `extra`",
+    parse_with = "parse_arch"
     )]
-    Arch(String, Option<Vec<String>>),
+    Arch(Option<String>, Option<Vec<String>>),
 
     #[command(description = "*/google* `question`")]
     Google(String),
 }
 
-fn parse_arch(input: String) -> Result<(String, Option<Vec<String>>), ParseError> {
+#[allow(clippy::unnecessary_wraps)]
+fn parse_arch(input: String) -> Result<(Option<String>, Option<Vec<String>>), ParseError> {
     if input.is_empty() {
-        return Err(ParseError::TooFewArguments {
-            expected: 1,
-            found: 0,
-            message: "at least 1".to_string(),
-        });
+        return Ok((None, None));
     }
 
     let mut cmd_with_args = input.split_whitespace();
@@ -29,9 +26,9 @@ fn parse_arch(input: String) -> Result<(String, Option<Vec<String>>), ParseError
     let args = cmd_with_args.map(String::from).collect::<Vec<_>>();
 
     if args.is_empty() {
-        Ok((cmd, None))
+        Ok((Some(cmd), None))
     } else {
-        Ok((cmd, Some(args)))
+        Ok((Some(cmd), Some(args)))
     }
 }
 
@@ -43,7 +40,7 @@ mod tests {
     fn parse_arch() {
         let cmd: Command = Command::parse("/arch linux", "").unwrap();
 
-        assert_eq!(cmd, Command::Arch("linux".to_string(), None));
+        assert_eq!(cmd, Command::Arch(Some("linux".to_string()), None));
     }
 
     #[test]
@@ -52,8 +49,15 @@ mod tests {
 
         assert_eq!(
             cmd,
-            Command::Arch("linux".to_string(), Some(vec!["testing".to_string()]))
+            Command::Arch(Some("linux".to_string()), Some(vec!["testing".to_string()]))
         );
+    }
+
+    #[test]
+    fn parse_arch_empty() {
+        let cmd: Command = Command::parse("/arch", "").unwrap();
+
+        assert_eq!(cmd, Command::Arch(None, None));
     }
 
     #[test]
@@ -61,5 +65,12 @@ mod tests {
         let cmd: Command = Command::parse("/google linux", "").unwrap();
 
         assert_eq!(cmd, Command::Google("linux".to_string()));
+    }
+
+    #[test]
+    fn parse_google_empty() {
+        let cmd: Command = Command::parse("/google", "").unwrap();
+
+        assert_eq!(cmd, Command::Google("".to_string()));
     }
 }
--- src/handle.rs
+++ src/handle.rs
@@ -11,6 +11,11 @@ use crate::google;
 
 static GOOGLE_KEY_AND_CX: SyncOnceCell<Option<(String, String)>> = SyncOnceCell::new();
 
+struct Answer {
+    result: String,
+    mode: ParseMode,
+}
+
 pub async fn handle(cx: UpdateWithCx<Message>, cmd: Command) -> ResponseResult<()> {
     let answer = match cmd {
         Command::Google(question) => {
@@ -26,64 +31,89 @@ pub async fn handle(cx: UpdateWithCx<Message>, cmd: Command) -> ResponseResult<(
             });
 
             if let Some((google_key, google_cx)) = google_key_and_cx {
-                google::search(&question, google_key, google_cx)
-                    .await
-                    .map(|result| result.to_string())
-                    .unwrap_or_else(|_| "哎呀咱好像在 Google 数据中心中迷路了~".to_string())
+                if question.is_empty() {
+                    Answer {
+                        result: "*/google* `question`".to_string(),
+                        mode: ParseMode::MarkdownV2,
+                    }
+                } else {
+                    let answer = google::search(&question, google_key, google_cx)
+                        .await
+                        .map(|result| result.to_string())
+                        .unwrap_or_else(|_| "哎呀咱好像在 Google 数据中心中迷路了~".to_string());
+
+                    Answer {
+                        result: answer,
+                        mode: ParseMode::HTML,
+                    }
+                }
             } else {
                 return Ok(());
             }
         }
 
         Command::Arch(name, repos) => {
-            let repos = match repos {
-                None => vec![],
-                Some(repos) => repos
-                    .into_iter()
-                    .filter(|repo| !repo.is_empty())
-                    .collect::<Vec<String>>(),
-            };
+            match name {
+                None => Answer {
+                    result: "*/arch* `package [repo]`, repo: eg: `stable` , `testing`, `aur` or `core` , `extra`".to_string(),
+                    mode: ParseMode::MarkdownV2,
+                },
+                Some(name) => {
+                    let repos = match repos {
+                        None => vec![],
+                        Some(repos) => repos
+                            .into_iter()
+                            .filter(|repo| !repo.is_empty())
+                            .collect::<Vec<String>>(),
+                    };
 
-            let repos = repos.iter().map(|repo| repo.as_str()).collect::<Vec<_>>();
+                    let repos = repos.iter().map(|repo| repo.as_str()).collect::<Vec<_>>();
 
-            if repos.contains(&"aur") || repos.contains(&"AUR") {
-                arch::aur_query(&name)
-                    .await
-                    .map(|result| result.to_string())
-                    .unwrap_or_else(|err| {
-                        log::error!("{}", err);
+                    let answer = if repos.contains(&"aur") || repos.contains(&"AUR") {
+                        arch::aur_query(&name)
+                            .await
+                            .map(|result| result.to_string())
+                            .unwrap_or_else(|err| {
+                                log::error!("{}", err);
 
-                        "哎呀咱好像在 AUR 数据库中迷路了~".to_string()
-                    })
-            } else if let Ok(result) = arch::official_query(&name, &repos).await {
-                if !result.is_empty() {
-                    result.to_string()
-                } else {
-                    arch::aur_query(&name)
-                        .await
-                        .map(|result| {
-                            if result.is_empty() {
-                                "咱没有找到结果，并且不是咱吃了！！！".to_string()
-                            } else {
-                                result.to_string()
-                            }
-                        })
-                        .unwrap_or_else(|err| {
-                            log::error!("{}", err);
+                                "哎呀咱好像在 AUR 数据库中迷路了~".to_string()
+                            })
+                    } else if let Ok(result) = arch::official_query(&name, &repos).await {
+                        if !result.is_empty() {
+                            result.to_string()
+                        } else {
+                            arch::aur_query(&name)
+                                .await
+                                .map(|result| {
+                                    if result.is_empty() {
+                                        "咱没有找到结果，并且不是咱吃了！！！".to_string()
+                                    } else {
+                                        result.to_string()
+                                    }
+                                })
+                                .unwrap_or_else(|err| {
+                                    log::error!("{}", err);
 
-                            "哎呀咱好像在 AUR 数据库中迷路了~".to_string()
-                        })
+                                    "哎呀咱好像在 AUR 数据库中迷路了~".to_string()
+                                })
+                        }
+                    } else {
+                        "哎呀咱好像在 Archlinux 数据库中迷路了~".to_string()
+                    };
+
+                    Answer {
+                        result: answer,
+                        mode: ParseMode::HTML,
+                    }
                 }
-            } else {
-                "哎呀咱好像在 Archlinux 数据库中迷路了~".to_string()
             }
         }
     };
 
     let message_id = cx.update.id;
 
-    cx.reply_to(answer)
-        .parse_mode(ParseMode::HTML)
+    cx.reply_to(answer.result)
+        .parse_mode(answer.mode)
         .reply_to_message_id(message_id)
         .send()
         .await?;
